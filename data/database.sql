62dfd09b8015339f13e020af2db5f8d512c955f6
Gustavo Girardon <ggirardon@gmail.com>
2024-10-23 23:26:10 -0300

    CI: added github actions step

--- data/database.sql
+++ data/database.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS user_account (
                                             id INT AUTO_INCREMENT PRIMARY KEY,
-                                            name VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+                                            username VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    premiumdays INT DEFAULT 0,
-    premiumdays_purchased INT DEFAULT 0,
-    type INT DEFAULT 0,
-    coins INT DEFAULT 0,
-    coins_transferable INT DEFAULT 0,
-    data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
-    data_ultima_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    data_delecao DATETIME NULL
+    premium_days INT DEFAULT 0,
+    premium_days_purchased INT DEFAULT 0,
+    account_type INT DEFAULT 0,
+    balance_coins INT DEFAULT 0,
+    transferable_coins INT DEFAULT 0,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at DATETIME NULL
     );
